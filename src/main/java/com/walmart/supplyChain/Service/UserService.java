34bab4560d9a097af1af686577641ced7603e75a
Manasi Deshmukh <manasi.deshmukh@cumminscollege.in>
2024-08-05 17:05:07 +0530

    .

--- src/main/java/com/walmart/supplyChain/Service/UserService.java
+++ src/main/java/com/walmart/supplyChain/Service/UserService.java
@@ -5,6 +5,8 @@ import com.walmart.supplyChain.Dto.LoginDto;
 import com.walmart.supplyChain.Dto.UserDto;
 
 public interface UserService {
+
     String addUser(UserDto userDto);
+
     LoginResponse loginUser(LoginDto loginDto);
 }
